557454e2c5c4225278da4292e11adad1ed02b416
Burtsev Alexander <Alexander.burzev@gmail.com>
2020-10-06 14:30:38 +0300

    fix(project): migrate production

--- local/modules/app.integration/lib/handlers/order.php
+++ local/modules/app.integration/lib/handlers/order.php
@@ -4,9 +4,10 @@ namespace App\Integration\Handlers;
 
 use App\Integration;
 use Bitrix\Main;
-use Bitrix\Sale;
 use Bitrix\Main\Loader;
+use Bitrix\Sale\Delivery;
 use Bitrix\Main\Localization\Loc;
+use Bitrix\Sale;
 use Bitrix\Sale\Internals\OrderPropsValueTable;
 
 Loc::loadMessages(__FILE__);
@@ -36,57 +37,72 @@ class Order
         $result = OrderPropsValueTable::getList([
             'select' => ['ID', 'NAME', 'VALUE', 'CODE', 'ORDER_PROPS_ID'],
             'filter' => ['ORDER_ID' => $order->getId()],
-         ]);
+        ]);
         while ($row = $result->fetch()) {
             $propertyValues[$row['CODE']] = $row;
-		}
+        }
 
-		// собираем по продукции
-		$arrBaskets = [];
-		foreach ($order->getBasket() as $basketItem) {
-			$arrProperty = [];
-			$basketPropRes = Sale\Internals\BasketPropertyTable::getList(array(
-				'filter' => array(
-				   "BASKET_ID" => $basketItem->getId(),
-				),
-			 ));
-			 
-			 while ($property = $basketPropRes->fetch()) {
-				$arrProperty[$property['CODE']] = $property['VALUE'];
-			 }
+        // собираем по продукции
+        $arrBaskets = [];
+        foreach ($order->getBasket() as $basketItem) {
+            $arrProperty = [];
+            $basketPropRes = Sale\Internals\BasketPropertyTable::getList([
+                'filter' => [
+                    'BASKET_ID' => $basketItem->getId(),
+                ],
+            ]);
 
-			$arrBaskets[] = [
-				"id" => $basketItem->getField('ID'),
-				"name" => $basketItem->getField('NAME'),
-				"quantity" => $basketItem->getQuantity(),
-				"price" => $basketItem->getPrice(),
-				"sum" => $basketItem->getFinalPrice(),
-				"offerId" => $basketItem->getProductId(),
-				"property" => $arrProperty
-			];	
+            while ($property = $basketPropRes->fetch()) {
+                $arrProperty[$property['CODE']] = $property['VALUE'];
+            }
+
+            $arrBaskets[] = [
+                'id' => $basketItem->getField('ID'),
+                'name' => $basketItem->getField('NAME'),
+                'quantity' => $basketItem->getQuantity(),
+                'price' => $basketItem->getPrice(),
+                'sum' => $basketItem->getFinalPrice(),
+                'offerId' => $basketItem->getProductId(),
+                'property' => $arrProperty
+            ];
+        }
+
+		/*Shipment*/
+		$arrShipment = [];
+		$shipmentCollection = $order->getShipmentCollection();
+		foreach ($shipmentCollection as $shipment)
+		{
+			$field =$shipment->getFields()->getValues();
+			$arrShipment[] = $field['DELIVERY_NAME'];
 		}
+		$arrShipment = array_unique($arrShipment);
+
+        $propertyCollection = $order->getPropertyCollection();
 
         // передача запроса в CRM
         $fields = [
-			'id' => $order->getId(),
-			'statusId' => $order->getField('STATUS_ID'),
-			'price' => $order->getPrice(),
-			'dateInsert' => $order->getDateInsert()->format('d.m.Y H:i:s'),
-			'discoutPrice' => $order->getDiscountPrice(),
-			'currency' => $order->getCurrency(),
-			'basket' => $arrBaskets,
-			'profile' => [
-				'fullName' => $propertyValues['FIO']['VALUE'],
-				'email' => $propertyValues['EMAIL']['VALUE'],
-				'phone' => $propertyValues['PHONE']['VALUE'],
-				'zip' => $propertyValues['ZIP']['VALUE'],
-				'city' => $propertyValues['CITY']['VALUE'],
-				'address' => $propertyValues['ADDRESS']['VALUE'],
-			]
-		];
+            'id' => $order->getId(),
+            'statusId' => $order->getField('STATUS_ID'),
+            'price' => $order->getPrice(),
+            'priceDelivery' => $order->getDeliveryPrice(),
+            'delivery' => implode(" ", $arrShipment),
+            'dateInsert' => $order->getDateInsert()->format('d.m.Y H:i:s'),
+            'discoutPrice' => $order->getDiscountPrice(),
+            'currency' => $order->getCurrency(),
+            'basket' => $arrBaskets,
+            'property' => $propertyCollection->getArray(),
+            'profile' => [
+                'fullName' => $propertyValues['FIO']['VALUE'],
+                'email' => $propertyValues['EMAIL']['VALUE'],
+                'phone' => $propertyValues['PHONE']['VALUE'],
+                'zip' => $propertyValues['ZIP']['VALUE'],
+                'city' => $propertyValues['CITY']['VALUE'],
+                'address' => $propertyValues['ADDRESS']['VALUE'],
+            ]
+        ];
 
         $endpoint = new Integration\Rest\Client\Crm();
-		$response = $endpoint->order("add", $fields);
+        $response = $endpoint->order('add', $fields);
 
         return true;
     }
--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,104 +1,151 @@
 <?php
+include_once $_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/wsrubi.smtp/classes/general/wsrubismtp.php';
 
-include_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/wsrubi.smtp/classes/general/wsrubismtp.php");
+require_once $_SERVER['DOCUMENT_ROOT'].'/local/lib/IblockElementPropertyTable.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/local/lib/CSms4bBase.php';
 
-define("LOG_FILENAME", $_SERVER["DOCUMENT_ROOT"]."/log.txt");
+define('LOG_FILENAME', $_SERVER['DOCUMENT_ROOT'].'/log.txt');
 
-require_once('app_init.php');
+require_once 'app_init.php';
 
-if (!function_exists('p')) {
-	function p($arr, $admin = false)
-	{
-		global $USER;
-		$message = '<pre style="font-size: 10pt; background-color: #fff; color: #000; margin: 10px; padding: 10px; border: 1px solid red; text-align: left; max-width: 800px; max-height: 600px; overflow: scroll">' . print_r($arr, true) . '</pre>';
-		if ($admin) {
-			if ($USER->IsAdmin()) {
-				echo $message;
-			}
-		} else {
-			echo $message;
-		}
-	}
+if (! function_exists('p')) {
+    function p($arr, $admin = false)
+    {
+        global $USER;
+        $message = '<pre style="font-size: 10pt; background-color: #fff; color: #000; margin: 10px; padding: 10px; border: 1px solid red; text-align: left; max-width: 800px; max-height: 600px; overflow: scroll">'.print_r($arr, true).'</pre>';
+        if ($admin) {
+            if ($USER->IsAdmin()) {
+                echo $message;
+            }
+        } else {
+            echo $message;
+        }
+    }
 }
 
-if (!function_exists('p2f')) {
-	function p2f($obj, $admOnly = true)
-	{
-		global $USER;
+if (! function_exists('p2f')) {
+    function p2f($obj, $admOnly = true)
+    {
+        global $USER;
 
-		if (!is_object($USER))
-			$USER = new \CUser();
+        if (! is_object($USER)) {
+            $USER = new \CUser();
+        }
 
-		if ($USER->IsAdmin() || $admOnly === false) {
-			if ($admOnly) $userID = 1; else $userID = $USER->GetID();
-			if (IntVal($userID) == 0) {
-				$userID = 'none';
-			}
-			$dump = "<pre style='font-size: 11px; font-family: tahoma;'>" . print_r($obj, true) . "</pre>";
-			$files = $_SERVER["DOCUMENT_ROOT"] . "/" . $userID . "-dump.html";
-			$fp = fopen($files, "a+");
-			fwrite($fp, $dump);
-			fclose($fp);
-		}
-	}
+        if ($USER->IsAdmin() || $admOnly === false) {
+            if ($admOnly) {
+                $userID = 1;
+            } else {
+                $userID = $USER->GetID();
+            }
+            if (IntVal($userID) == 0) {
+                $userID = 'none';
+            }
+            $dump = "<pre style='font-size: 11px; font-family: tahoma;'>".print_r($obj, true).'</pre>';
+            $files = $_SERVER['DOCUMENT_ROOT'].'/'.$userID.'-dump.html';
+            $fp = fopen($files, 'a+');
+            fwrite($fp, $dump);
+            fclose($fp);
+        }
+    }
 }
 
-if (!function_exists('p2log')) {
-	function p2log($obj, $key = '')
-	{
-		if (empty($key)) {
-			$key = 'main';
-		}
+if (! function_exists('p2log')) {
+    function p2log($obj, $key = '')
+    {
+        if (empty($key)) {
+            $key = 'main';
+        }
 
-		$dump = print_r($obj, true) . "\r\n";
-		$files = $_SERVER["DOCUMENT_ROOT"] . "/upload/log/" . $key . ".log";
-		$fp = fopen($files, "a+");
-		fwrite($fp, $dump);
-		fclose($fp);
-	}
+        $dump = print_r($obj, true)."\r\n";
+        $files = $_SERVER['DOCUMENT_ROOT'].'/upload/log/'.$key.'.log';
+        $fp = fopen($files, 'a+');
+        fwrite($fp, $dump);
+        fclose($fp);
+    }
 }
 
-if (!function_exists('pr')) {
-	/**
-	 * Выводит информацию об объекте в стилизованном, удобочитаемом виде.
-	 * C выводом отображения строки откуда вызывана
-	 * @param $o
-	 * @param bool $option true: показывать стек вызовов
-	 * @param bool $stack true: var_dump, false: print_r
-	 */
-	function pr($o, $option = false, $stack = false)
-	{
-		$bt = debug_backtrace();
-		$bt = $bt[0];
-		$dRoot = $_SERVER["DOCUMENT_ROOT"];
-		$dRoot = str_replace("/", "\\", $dRoot);
-		$bt["file"] = str_replace($dRoot, "", $bt["file"]);
-		$dRoot = str_replace("\\", "/", $dRoot);
-		$bt["file"] = str_replace($dRoot, "", $bt["file"]);
+if (! function_exists('pr')) {
+    /**
+     * Выводит информацию об объекте в стилизованном, удобочитаемом виде.
+     * C выводом отображения строки откуда вызывана.
+     * @param $o
+     * @param bool $option true: показывать стек вызовов
+     * @param bool $stack true: var_dump, false: print_r
+     */
+    function pr($o, $option = false, $stack = false)
+    {
+        $bt = debug_backtrace();
+        $bt = $bt[0];
+        $dRoot = $_SERVER['DOCUMENT_ROOT'];
+        $dRoot = str_replace('/', '\\', $dRoot);
+        $bt['file'] = str_replace($dRoot, '', $bt['file']);
+        $dRoot = str_replace('\\', '/', $dRoot);
+        $bt['file'] = str_replace($dRoot, '', $bt['file']);
 
-		if (php_sapi_name() != 'cli') {
-			?>
+        if (php_sapi_name() != 'cli') {
+            ?>
 			<div style='font-size:9pt; color:#000; background:#fff; border:1px dashed #000;'>
-				<div style='padding:3px 5px; background:#99CCFF; font-weight:bold;'>File: <?= $bt["file"] ?>
-					[<?= $bt["line"] ?>]
+				<div style='padding:3px 5px; background:#99CCFF; font-weight:bold;'>File: <?= $bt['file'] ?>
+					[<?= $bt['line'] ?>]
 				</div>
-				<? if ($option) { ?>
-					<pre style='padding:10px;'><? var_dump(!$stack ? $o : debug_backtrace()); ?></pre>
-				<? } else { ?>
-					<pre style='padding:10px;'><? print_r(!$stack ? $o : debug_backtrace()); ?></pre>
-				<? } ?>
+				<?php if ($option) { ?>
+					<pre style='padding:10px;'><?php var_dump(! $stack ? $o : debug_backtrace()); ?></pre>
+				<?php } else { ?>
+					<pre style='padding:10px;'><?php print_r(! $stack ? $o : debug_backtrace()); ?></pre>
+				<?php } ?>
 
 			</div>
-		<? } else {
-			fprintf("File: %s \n ______________________ \n Count: %s", $bt['file']);
-			print_r($o);
-		} ?>
 		<?php
-	}
+        } else {
+            fprintf("File: %s \n ______________________ \n Count: %s", $bt['file']);
+            print_r($o);
+        } ?>
+		<?php
+    }
 }
 
+AddEventHandler('sale', 'OnOrderAdd', 'OnOrderAddHandler');
 
+function OnOrderAddHandler(&$arFields)
+{
+    //отправка SMS
+    if (isset($arFields['ORDER_PROP'][3])) {
+        $phone = $arFields['ORDER_PROP'][3];
+    } else {
+        $phone = '';
+    }
+    sendSMS($phone, $arFields['PRICE']);
+}
 
+function sendSMS($phone, $price)
+{
+    $LOGIN = 'Abramova';
+    $PASSWORD = '1cbit-abramova';
+
+    $messagePers = 'Новый заказ на сайте Eclair Cafe';
+    $SMS4B = new \Csms4bBase($LOGIN, $PASSWORD);
+
+    if (strlen($phone) > 0) {
+        $messageclient = 'Ваш заказ принят. Сумма заказа '.$price.'. Мы свяжемся с вами в ближайшее время';
+        $smsResult = $SMS4B->SendSMS($messageclient, $phone, 'ECLAIR CAFE');
+    }
+
+    $smsResult = $SMS4B->SendSMS($messagePers, '79833235321', 'ECLAIR CAFE');
+    $smsResult = $SMS4B->SendSMS($messagePers, '79831242541', 'ECLAIR CAFE');
+    $smsResult = $SMS4B->SendSMS($messagePers, '79130190362', 'ECLAIR CAFE');
+    $smsResult = $SMS4B->SendSMS($messagePers, '79134761496', 'ECLAIR CAFE');
+    $smsResult = $SMS4B->SendSMS($messagePers, '79529053533', 'ECLAIR CAFE');
+}
+
+function show($arr)
+{
+    print '<pre>';
+    print_r($arr);
+    print '</pre>';
+}
+
+/*
 Bitrix\Main\EventManager::getInstance()->addEventHandler(
     'sale',
     'onSaleDeliveryRestrictionsClassNamesBuildList',
@@ -114,3 +161,4 @@ function myDeliveryFunction()
         )
     );
 }
+*/
